fdbd2d5e912116acc27ed864df7086b8da49b4d0
willwilson <qtvhao@gmail.com>
2024-10-03 17:34:55 +0700

    Refactor app.module.ts to add @nestjs/microservices dependency and configure Kafka service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app/app.gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: [process.env.KAFKA_BROKERS], // Địa chỉ Kafka broker
+          },
+          consumer: {
+            groupId: 'notification-consumer', // Group ID cho Kafka consumer
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
 })

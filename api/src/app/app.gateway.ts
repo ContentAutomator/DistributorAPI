df5e6d9b233125eb9d07e7db160b1555a428ca72
willwilson <qtvhao@gmail.com>
2024-10-03 15:37:46 +0700

    Refactor WebSocketGateway configuration and add client connection and disconnection handling

--- api/src/app/app.gateway.ts
+++ api/src/app/app.gateway.ts
@@ -1,9 +1,28 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({
+  cors: {
+    origin: '*', // Cho phép tất cả các nguồn gốc (có thể điều chỉnh để bảo mật hơn)
+  },
+})
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+  private intervalId: NodeJS.Timeout;
+
+  handleConnection(client: any) {
+    console.log('Client connected:', client.id);
+
+    // Gửi thông báo cho client mỗi 30 giây
+    this.intervalId = setInterval(() => {
+      this.server.emit('notification', { message: 'This is a notification sent every 30 seconds!' });
+    }, 30000);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('Client disconnected:', client.id);
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }

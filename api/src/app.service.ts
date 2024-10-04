5fe211d567d4ae22bcdf97c87aec633a9c239997
willwilson <qtvhao@gmail.com>
2024-10-04 12:54:03 +0700

    Refactor AppService to handle Kafka messages and send notifications to clients with matching secret keys

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -17,23 +17,26 @@ export class AppService implements OnModuleInit {
     console.log('Subscribed to notification-topic');
     // await this.kafkaClient.connect();
     console.log('Connected to Kafka');
-    // KafkaJS v2.0.0 switched default partitioner. 
-    // To retain the same partitioning behavior as in previous versions, create the producer with the option "createPartitioner: Partitioners.LegacyPartitioner". 
-    // See the migration guide at https://kafka.js.org/docs/migration-guide-v2.0.0#producer-new-default-partitioner for details. 
+    // KafkaJS v2.0.0 switched default partitioner.
+    // To retain the same partitioning behavior as in previous versions, create the producer with the option "createPartitioner: Partitioners.LegacyPartitioner".
+    // See the migration guide at https://kafka.js.org/docs/migration-guide-v2.0.0#producer-new-default-partitioner for details.
     // Silence this warning by setting the environment variable "KAFKAJS_NO_PARTITIONER_WARNING=1" {"timestamp":"2024-10-03T14:02:37.281Z","logger":"kafkajs"}
   }
 
   // Nhận tin nhắn từ Kafka
-  async handleMessage(job: { 
-    job_id: string, 
-    status: string, 
-    video_url: string, 
-    thumbnail_url: string, 
-    message: string, 
-    duration: number, 
-    resolution: string, 
-    format: string 
-  }, secret_key: string) {
+  async handleMessage(
+    job: {
+      job_id: string;
+      status: string;
+      video_url: string;
+      thumbnail_url: string;
+      message: string;
+      duration: number;
+      resolution: string;
+      format: string;
+    },
+    secret_key: string,
+  ) {
     console.log('Received message from Kafka:', job);
 
     // Gửi thông báo tới tất cả client qua WebSocket
@@ -43,7 +46,10 @@ export class AppService implements OnModuleInit {
     return 'Hello World!';
   }
 
-  renderVideo(videoDetails: VideoDetailsDto): { message: string; job_id: string } {
+  renderVideo(videoDetails: VideoDetailsDto): {
+    message: string;
+    job_id: string;
+  } {
     return {
       message: 'Video rendering job submitted successfully',
       job_id: '12345',
